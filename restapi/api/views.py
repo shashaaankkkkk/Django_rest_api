a1d6efcbe78ec62ac4596c410092b14584ed2eed
shashaaankkkkk <shashankshekhar8534@gmail.com>
2023-11-14 18:20:26 +0530

    added requirements.txt

--- restapi/api/views.py
+++ restapi/api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse , JsonResponse
+from .models import name
+from .Serializers import namese
 # Create your views here.
+
+def hello(request):
+    return HttpResponse("hello")
+def api(request):
+    data1=name.objects.all()
+    serial=namese(data1,many=True)
+    return JsonResponse(serial.data,safe=False)
